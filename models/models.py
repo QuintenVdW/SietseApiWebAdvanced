6a5333cb58fbb6fda51539fc1a64e39f89247608
QuintenVdW <104367615+QuintenVdW@users.noreply.github.com>
2024-05-02 22:56:53 +0200

    Create models.py

---
+++ models/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+class ExampleModel(Base):
+    tablename = "example"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    Description = Column(String, index=True)
